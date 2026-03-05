eb8e6eba88c936523d812bb7afbe5fe9a8678c90
Leonel Cabrera <leoancab@espol.edu.ec>
2026-03-05 14:15:43 -0500

    estructura inicial del frontend

---
+++ frontend/src/pages/Login.jsx
@@ -0,0 +1,17 @@
+function Login() {
+  return (
+    <div>
+      <h1>Login</h1>
+
+      <input placeholder="Email" />
+      <br />
+
+      <input placeholder="Password" type="password" />
+      <br />
+
+      <button>Ingresar</button>
+    </div>
+  )
+}
+
+export default Login
